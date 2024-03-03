e7c299df9d82f8bf8f2e4d3bc4a2600c7d30c412
Leah Torregiano <lct2986@rit.edu>
2024-03-02 23:08:30 -0500

    Fix compilation errors for ariel?

--- Match Girl/Assets/Scripts/Controllers/BottomBarController.cs
+++ Match Girl/Assets/Scripts/Controllers/BottomBarController.cs
@@ -11,10 +11,37 @@ public class BottomBarController : MonoBehaviour
     private int sentenceIndex = -1;
     private StoryScene currentScene;
     private State state = State.COMPLETE;
+    private Animator animator;
+    private bool isHidden = false;
 
     private enum State
     {
         PLAYING, COMPLETE
+    }
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Hide()
+    {
+        if (!isHidden)
+        {
+            animator.SetTrigger("Hide");
+            isHidden = true;
+        }
+    }
+
+    public void Show()
+    {
+        animator.SetTrigger("Show");
+        isHidden = false;
+    }
+
+    public void ClearText()
+    {
+        barText.text = "";
     }
 
     public void PlayScene(StoryScene scene)
